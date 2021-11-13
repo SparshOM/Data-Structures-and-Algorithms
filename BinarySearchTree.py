b66c4348f14da1503b0370ca8114e347c658b22f
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2021-11-13 15:46:34 +0530

    Update BinarySearchTree.py

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -19,8 +19,47 @@ class bst:
                 else:
                     crrNode =crrNode.right
         return self
+    # def inorder(self):
+        
+    #     if self.left:
+    #         self.left.inorder()
+    #     print( self.value)
+    #     if self.right:
+    #         self.right.inorder()
+
+    # def preorder(self):
+        
+    #     print(self.value)
+    #     if self.left:
+    #         self.left.preorder()
+    #     if self.right:
+    #         self.right.preorder()
+    
+            
+    # def postorder(self):
+    #     if self.left:
+    #         self.left.postorder()
+    #     if self.right:
+    #         self.right.postorder()
+    #     print(self.value)
+
+    def Search(self,target):
+        crrnode = self
+        if crrnode.value<target:
+            crrnode = crrnode.right
+        elif crrnode.value>target:
+            crrnode = crrnode.left
+        print(crrnode.value ==target)
+
 root= bst(10)
 root.insert(5)
 root.insert(15)
 root.insert(2)
-root.insert(22)
+root.insert(1)
+root.insert(12)
+root.insert(14)
+root.insert(75)
+# root.inorder()
+# root.preorder()
+# root.postorder()
+root.Search(10)
